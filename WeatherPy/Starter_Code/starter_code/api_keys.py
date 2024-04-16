a133f698806af91a83e3a887bb35062de11c7ef6
Melina Hernandez <melinahernandeze@gmail.com>
2024-04-16 01:38:34 -0400

    final

--- WeatherPy/Starter_Code/starter_code/api_keys.py
+++ WeatherPy/Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "29036135bd9e6ac435bc84ab11786675"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "4d8dbdf15823435ba295e2f5e55684f7"
